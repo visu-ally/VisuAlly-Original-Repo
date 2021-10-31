63fbc88a589f24b2f8cc4bd22b7c920ba8db756c
Kevin Naveen <knave059@uottawa.ca>
2021-10-31 11:30:14 -0400

    Added contact form to the contact page

--- Temp.js
+++ Temp.js
@@ -1,5 +1,5 @@
 const tl = gsap.timeline({defaults: {ease: 'power1.out'} } );
 
 tl.to(".slider", { y: "-100%", duration:1, delay: 0.5});
-tl.to(".intro", {y: "-100%", duration: 0.5}, "-=1")
+tl.to(".intro", {y: "-100%", duration: 0.6}, "-=1")
 tl.fromTo()
